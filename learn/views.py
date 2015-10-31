799f008dae9e85d7dbe9895e52dfdf9ef463954f
dongm2ez <dongm2ez@163.com>
2015-10-31 13:26:48 +0800

    对微信接入方法进行bug修复
    对微信接入方法进行修正,以正确接入

--- learn/views.py
+++ learn/views.py
@@ -6,22 +6,20 @@ import hashlib
 # index方法
 def index(request):
     #获取微信传递参数
-    signature = request.GET['signature']
-    timestamp = request.GET['timestamp']
-    nonce     = request.GET['nonce']
-    echostr   = request.GET['echostr']
+    signature   = request.GET.get('signature', None)
+    timestamp   = request.GET.get('timestamp', None)
+    nonce       = request.GET.get('nonce', None)
+    echostr     = request.GET.get('echostr', None)
     #微信自己设置的token
-    token     = 'life'
-    #字典排序
-    list      = [signature, timestamp, nonce]
-    list.sort()
-    sha1      = hashlib.sha1()
-    map(sha1.update, list)
-    hashcode  = sha1.hexdigest()
-    #sha1加密算法
-
+    token       = 'life'
+    #将token、timestamp、nonce三个参数进行字典序排序
+    tmpList     = [token, timestamp, nonce]
+    tmpList.sort()
+    #将三个参数字符串拼接成一个字符串进行sha1加密
+    tmpStr      = "%s%s%s" % tuple(tmpList)
+    hashcode    = hashlib.sha1(tmpStr).hexdigest()
     #如果是来自微信请求，则回复echostr
     if hashcode == signature:
         return HttpResponse(echostr)
     else:
-        return HttpResponse('')
+        return HttpResponse('weixin  index')
