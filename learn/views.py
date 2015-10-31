8c5083a93fc9402932ef02571b63a5d3b034153d
dongm2ez <dongm2ez@163.com>
2015-10-31 09:40:05 +0800

    微信接入
    微信接入方法

--- learn/views.py
+++ learn/views.py
@@ -1,7 +1,27 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+import hashlib
 
 # index方法
 def index(request):
-    return HttpResponse('Hello world')
+    #获取微信传递参数
+    signature = request.GET['signature']
+    timestamp = request.GET['timestamp']
+    nonce     = request.GET['nonce']
+    echostr   = request.GET['echostr']
+    #微信自己设置的token
+    token     = 'life'
+    #字典排序
+    list      = [signature, timestamp, nonce]
+    list.sort()
+    sha1      = hashlib.sha1()
+    map(sha1.update, list)
+    hashcode  = sha1.hexdigest()
+    #sha1加密算法
+
+    #如果是来自微信请求，则回复echostr
+    if hashcode == signature:
+        return HttpResponse(echostr)
+    else:
+        return HttpResponse('')
