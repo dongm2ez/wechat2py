a7c0933a34e67150486023815071cf29a0f84fce
dongm2ez <dongm2ez@163.com>
2015-11-01 13:02:04 +0800

    完成获取用户信息并输出功能

--- learn/views.py
+++ learn/views.py
@@ -36,14 +36,26 @@ def index(request):
         else:
             return HttpResponse('wechat  index')
     elif request.method == 'POST':
+        # 解析本次xml数据
         try:
             wechat.parse_data(data = request.body)
         except ParseError:
             return HttpResponseBadRequest('Invalid XML Data')
+        # 获取解析好的xml数据
         message = wechat.get_message()
+        # 关注事件或者默认不匹配回复内容
         response = wechat.response_text(u'感谢您的关注！')
+        # 类型是文字回复内容
         if message.type == 'text':
-            response = wechat.response_text(u'^_^')
+            if message.content == '1':
+                openid   = message.source
+                userInfo = wechat.get_user_info(openid)
+                tmpStr = ''
+                for key, value in userInfo.items():
+                    tmpStr += "\"%s\":\"%s\"\"\n" % (key, value)
+                response = wechat.response_text(tmpStr)
+            else:
+                response = wechat.response_text(u'回复【1】查看您的信息')
         return HttpResponse(response, content_type = "application/xml")
     else:
         return HttpResponse('')
