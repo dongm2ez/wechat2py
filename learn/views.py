c57e657b00540a28bf4c29b1b9e6c6d0484ecb2f
dongm2ez <dongm2ez@163.com>
2015-11-01 14:51:59 +0800

    完成网页授权获得用户信息

--- learn/views.py
+++ learn/views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import hashlib
+import urllib
+import urllib2
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -50,12 +52,38 @@ def index(request):
             if message.content == '1':
                 openid   = message.source
                 userInfo = wechat.get_user_info(openid)
-                tmpStr = ''
+                tmpStr   = ''
                 for key, value in userInfo.items():
-                    tmpStr += "\"%s\":\"%s\"\"\n" % (key, value)
+                    tmpStr += "%s:%s\n" % (key, value)
                 response = wechat.response_text(tmpStr)
+            elif message.content == '2':
+                # 网页授权获得用户信息
+                oauth_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + AppID + "&redirect_uri=http://www.m2ez.com/info/&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect"
+                response = wechat.response_text(oauth_url)
             else:
-                response = wechat.response_text(u'回复【1】查看您的信息')
+                response = wechat.response_text(u'回复【1】查看您的信息\n回复【2】网页授权获得信息')
         return HttpResponse(response, content_type = "application/xml")
     else:
         return HttpResponse('')
+
+def info(request):
+    # 获取微信返回的code
+    code = request.GET.get('code', None);
+    accessTokenUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" + AppID + "&secret=" + AppSecret + "&code=" + code + "&grant_type=authorization_code"
+    # 获取网页返回信息
+    req         = urllib2.Request(accessTokenUrl)
+    resData     = urllib2.urlopen(req)
+    res         = resData.read()
+    # access_token转换为字典
+    accessDict  = eval(res)
+    userInfoUrl = "https://api.weixin.qq.com/sns/userinfo?access_token=" + accessDict['access_token'] + "&openid=" + accessDict['openid'] + "&lang=zh_CN"
+    # 获取网页返回信息
+    req         = urllib2.Request(userInfoUrl)
+    resData     = urllib2.urlopen(req)
+    res         = resData.read()
+    # 用户信息转换为字典
+    userInfoDict  = eval(res)
+    tmpStr   = ''
+    for key, value in userInfoDict.items():
+        tmpStr += "<h1>%s:%s</h1>" % (key, value)
+    return HttpResponse(tmpStr)
--- wechat2py/urls.py
+++ wechat2py/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^learn/', 'learn.views.index'),
+    url(r'^info/', 'learn.views.info'),
 ]
